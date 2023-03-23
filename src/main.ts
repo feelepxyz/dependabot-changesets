a649f226d845ec1a7a85fead0ac1b2a9717a4327
Philip Harrison <philip@mailharrison.com>
2023-03-23 09:53:18 +0000

    Fix changelog generator
    Signed-off-by: Philip Harrison <philip@mailharrison.com>

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import fs from 'fs'
-import path from 'path'
-import * as core from '@actions/core'
-import { Changeset, Config, PackageJSON } from "@changesets/types";
+import fs from "fs";
+import path from "path";
+import * as core from "@actions/core";
+import { Changeset, Config, PackageJSON, VersionType } from "@changesets/types";
 import writeChangeset from "@changesets/write";
 import * as git from "@changesets/git";
 import { getPackages } from "@manypkg/get-packages";
+import getChangesets from "@changesets/read";
 import { read } from "@changesets/config";
 
 function isListablePackage(config: Config, packageJson: PackageJSON) {
@@ -22,56 +23,105 @@ function isListablePackage(config: Config, packageJson: PackageJSON) {
   return hasVersionField;
 }
 
-async function run(): Promise<void> {
+interface UpdatedDeps {
+  dependencyName: string;
+  prevVersion: string;
+  newVersion: string;
+}
+
+async function run(cwd = process.cwd()): Promise<void> {
   try {
-    const cwd = process.cwd()
-    console.log(cwd + " -- CWD")
+    core.debug(`Running in current working directory: ${cwd}`);
     if (!fs.existsSync(path.resolve(cwd, ".changeset"))) {
-      throw new Error("There is no .changeset folder.\n" +
+      throw new Error(
+        "There is no .changeset folder.\n" +
         "If this is the first time `changesets` have been used in this project, run `yarn changeset init` to get set up.\n" +
         "If you expected there to be changesets, you should check git history for when the folder was removed to ensure you do not lose any configuration."
       );
     }
-    const metadata: string = process.env.DEPENDABOT_METADATA || ''
-    console.log(metadata)
-    const updatedDeps = JSON.parse(metadata)
-    console.log(JSON.stringify(updatedDeps, null, 2))
+    const metadata: string = process.env.DEPENDABOT_METADATA || "";
+    const updatedDeps: UpdatedDeps[] = JSON.parse(metadata);
+    if (updatedDeps.length === 0) {
+      console.log(
+        "Found no dependencies that have been updated so no changelog will be generated."
+      );
+      return;
+    }
+    core.debug(
+      `Generating changelogs for updated dependencies:\n${JSON.stringify(
+        updatedDeps
+      )}`
+    );
     const packages: any = await getPackages(cwd);
+    core.debug(`Found local npm packages: ${JSON.stringify(packages)}`);
     if (packages.packages.length === 0) {
       throw new Error(
         `No packages found. You might have ${packages.tool} workspaces configured but no packages yet?`
-      )
+      );
     }
-    const config: Config = await read(cwd, packages);
-    console.log(JSON.stringify(config, null, 2))
+    const config = await read(cwd, packages);
+    core.debug(`Found changesets config: ${JSON.stringify(config)}`);
+    const changedChagesets = await getChangesets(cwd, config.baseBranch);
+    const changedSummaries = changedChagesets.map(
+      (changeset) => changeset.summary
+    );
+    core.debug(`Detected existing changelogs: ${changedSummaries.join(", ")}`);
+
     const changedPackages = await git.getChangedPackagesSinceRef({
       cwd,
       ref: config.baseBranch,
       changedFilePatterns: config.changedFilePatterns,
-    })
+    });
+    const listableChanges = changedPackages.filter((pkg) =>
+      isListablePackage(config, pkg.packageJson)
+    );
+    const changedPackagesNames = listableChanges.map(
+      (pkg) => pkg.packageJson.name
+    );
+    if (changedPackagesNames.length === 0) {
+      console.log(
+        "No packages have changed so no changelog will be generated."
+      );
+      return;
+    }
+    core.debug(
+      `Detected changed packages (since ref: ${config.baseBranch
+      }): ${changedPackagesNames.join(", ")}`
+    );
 
-    const changedPackagesNames = changedPackages
-      .filter((pkg) => isListablePackage(config, pkg.packageJson))
-      .map((pkg) => pkg.packageJson.name);
+    let summary;
+    if (updatedDeps.length === 1) {
+      const dep = updatedDeps[0];
+      summary = `Bump ${dep["dependencyName"]} from ${dep["prevVersion"]} to ${dep["newVersion"]}`;
+    } else {
+      const deps = updatedDeps
+        .map(({ dependencyName, prevVersion, newVersion }) => {
+          return `- ${dependencyName} from ${prevVersion} to ${newVersion}`;
+        })
+        .join("\n");
+      summary = `Bump dependencies:\n${deps}`;
+    }
+    if (changedSummaries.includes(summary)) {
+      console.log(
+        `Changelog with summary "${summary}" already exists so no changelog will be generated.`
+      );
+      return;
+    }
 
-    // TODO: handle multiple dependencies
-    const updatedDep = updatedDeps[0]
-    const type = updatedDep['updateType'].replace('version-update:semver-', '') || 'none'
-    const dependencyNames = updatedDep['dependencyName']
-    const previousVersion = updatedDep['prevVersion']
-    const newVersion = updatedDep['newVersion']
-    const summary = `Bump ${dependencyNames.join(', ')} from ${previousVersion} to ${newVersion}`
-    const releases = changedPackagesNames.map((name: string) => ({ name, type }))
+    const type = process.env.DEFAULT_SEMVER_UPDATE_TYPE || "patch";
+    const releases = changedPackagesNames.map((name: string) => ({
+      name,
+      type: type as VersionType,
+    }));
     const newChangeset: Changeset = {
       summary,
-      releases
-    }
+      releases,
+    };
     await writeChangeset(newChangeset, cwd);
-    core.debug(JSON.stringify(newChangeset, null, 2))
-    core.setOutput('time', new Date().toTimeString())
+    core.debug(JSON.stringify(newChangeset));
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
 
-run()
+run();
